21f96af577eb44f8f35c63336a64098ef2ae9212
Dimitrimm <40797429+Dimitrimm@users.noreply.github.com>
2020-10-10 10:51:21 -0300

    feature/ TimeController

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -11,16 +11,30 @@ public class TimeController : MonoBehaviour
     
     private void Start()
     {
-        
+        setTime(15);
+    }
+
+    private void setTimeText(float time){
+        int intTime = (int)time;
+        this.timeText.text = intTime.ToString();
     }
 
     private void Update()
     {
-        
+       if(this.time > 0)
+       {
+           this.time -= Time.deltaTime;
+           setTimeText(this.time);
+       }
+       else
+       {
+           timeOut();
+       }
     }
 
     private void setTime(float newTime) {
         this.time = newTime;
+        setTimeText(this.time);
     }
 
     private void timeOut() {
